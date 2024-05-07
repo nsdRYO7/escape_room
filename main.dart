9981af889a3b4e95ece5531573b9af97857688a5
nsdRYO7 <147719575+nsdRYO7@users.noreply.github.com>
2024-05-07 21:00:34 +0900

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,99 +1,115 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(EscapeGameApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class EscapeGameApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Escape Game',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyEscapeRoom(),
+      home: EscapeGameScreen(),
     );
   }
 }
-class MyEscapeRoom extends StatefulWidget {
-  const MyEscapeRoom({super.key});
 
+class EscapeGameScreen extends StatefulWidget {
   @override
-  _EscapeRoomState createState() => _EscapeRoomState(); // 修正
+  _EscapeGameScreenState createState() => _EscapeGameScreenState();
 }
 
-class _EscapeRoomState extends State<MyEscapeRoom> {
-  bool ballVisible = true; // ボールの表示状態を管理
-
-  // ボールを取るアクション
-  void takeBall() {
-    setState(() {
-      ballVisible = false; // ボールを非表示にする
-    });
-  }
-
-  // ボールを置くアクション
-  void placeBall() {
-    setState(() {
-      ballVisible = true; // ボールを表示する
-    });
-  }
+class _EscapeGameScreenState extends State<EscapeGameScreen> {
+  int currentRoom = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Escape Game'),
+      body: Stack(
+        children: [
+          buildRoom(currentRoom),
+          buildRoomDoor(),
+          buildNavigationButtons(),
+          buildClickableArea(),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            if (ballVisible)
-              GestureDetector(
-                onTap: () {
-                  // ボールをクリックしたときの処理
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return SimpleDialog(
-                        title: const Text('アクションを選んでください'),
-                        children: <Widget>[
-                          SimpleDialogOption(
-                            onPressed: () {
-                              // "取る" アクション
-                              Navigator.pop(context);
-                              takeBall();
-                            },
-                            child: const Text('取る'),
-                          ),
-                          SimpleDialogOption(
-                            onPressed: () {
-                              // "置く" アクション
-                              Navigator.pop(context);
-                              placeBall();
-                            },
-                            child: const Text('置く'),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                },
-                child: Container(
-                  width: 50, // ボールの幅
-                  height: 50, // ボールの高さ
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.red, // ボールの色
-                  ),
-                ),
-              ),
-          ],
+    );
+  }
+
+  Widget buildRoom(int roomIndex) {
+    List<String> roomImages = [
+      'assets/joy_room.jpg',
+      'assets/sorrow_room.jpg',
+      'assets/anger_room.jpg',
+      'assets/fun_room.jpg',
+    ];
+
+    return GestureDetector(
+      onTap: () {
+        // ここでは何も処理を行わない
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(roomImages[roomIndex]),
+            fit: BoxFit.contain,
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget buildRoomDoor() {
+    return Positioned(
+      top: MediaQuery.of(context).size.height / 2 - 20,
+      left: 10,
+      child: GestureDetector(
+        onTap: () {
+          int prevRoom = currentRoom - 1;
+          if (prevRoom < 0) {
+            prevRoom = 3; // 部屋の数 - 1
+          }
+          setState(() {
+            currentRoom = prevRoom;
+          });
+        },
+        child: Icon(Icons.arrow_back, color: Colors.red),
+      ),
+    );
+  }
+
+  Widget buildNavigationButtons() {
+    return Positioned(
+      top: MediaQuery.of(context).size.height / 2 - 20,
+      right: 10,
+      child: GestureDetector(
+        onTap: () {
+          int nextRoom = (currentRoom + 1) % 4; // 部屋の数
+          setState(() {
+            currentRoom = nextRoom;
+          });
+        },
+        child: Icon(Icons.arrow_forward, color: Colors.red),
+      ),
+    );
+  }
+
+  Widget buildClickableArea() {
+    return GestureDetector(
+      onTap: () {
+        // 画像内の特定の部分をクリックしたときの処理
+        // 現在の部屋が変わるようにする例
+        setState(() {
+          currentRoom = (currentRoom + 1) % 4; // 部屋の数
+        });
+      },
+      child: Container(
+        width: 100,
+        height: 100,
+        color: Colors.red.withOpacity(0.5),
+        margin: EdgeInsets.fromLTRB(50, 50, 0, 0), // 画像内のクリック領域
       ),
     );
   }
