35726f9284f303345b66d47a3aa52d09dcb1c12f
nsdRYO7 <147719575+nsdRYO7@users.noreply.github.com>
2024-05-21 21:30:08 +0900

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,45 +1,114 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(EscapeGameApp());
-}
+class Room extends StatelessWidget {
+  final String imageAssetPath;
+  final int roomId;
 
-class EscapeGameApp extends StatelessWidget {
+  Room({required this.imageAssetPath, required this.roomId});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(imageAssetPath),
+          fit: BoxFit.contain,
+        ),
       ),
-      home: EscapeGameScreen(),
+    );
+  }
+}
+
+class RoomDoor extends StatelessWidget {
+  final int prevRoomId;
+
+  RoomDoor({required this.prevRoomId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: MediaQuery.of(context).size.height / 2 - 20,
+      left: 10,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pop();
+        },
+        child: Icon(Icons.arrow_back, color: Colors.red),
+      ),
+    );
+  }
+}
+
+class NavigationButtons extends StatelessWidget {
+  final int currentRoomId;
+
+  NavigationButtons({required this.currentRoomId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: MediaQuery.of(context).size.height / 2 - 20,
+      right: 10,
+      child: Visibility(
+        visible: currentRoomId != 4, // 特殊部屋ではない場合は表示
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) {
+                int nextRoomId = (currentRoomId + 1) % 4;
+                return EscapeGameScreen(currentRoomId: nextRoomId);
+              },
+            ));
+          },
+          child: Icon(Icons.arrow_forward, color: Colors.red),
+        ),
+      ),
+    );
+  }
+}
+
+class ClickableArea extends StatelessWidget {
+  final int currentRoomId;
+
+  ClickableArea({required this.currentRoomId});
+
+  @override
+  Widget build(BuildContext context) {
+    if (currentRoomId != 0) {
+      return SizedBox.shrink(); // joy_roomでない場合は何も表示しない
+    }
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => EscapeGameScreen(
+            currentRoomId: 4,
+          ),
+        ));
+      },
+      child: Container(
+        width: 100,
+        height: 100,
+        color: Colors.red.withOpacity(0.5),
+        margin: EdgeInsets.fromLTRB(
+            MediaQuery.of(context).size.width / 2 - 50, 50, 0, 0), // 画像内のクリック領域を中央の少し左に配置
+      ),
     );
   }
 }
 
 class EscapeGameScreen extends StatefulWidget {
+  final int currentRoomId;
+
+  EscapeGameScreen({required this.currentRoomId});
+
   @override
   _EscapeGameScreenState createState() => _EscapeGameScreenState();
 }
 
 class _EscapeGameScreenState extends State<EscapeGameScreen> {
-  int currentRoom = 0;
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          buildRoom(currentRoom),
-          buildRoomDoor(),
-          buildNavigationButtons(),
-          buildClickableArea(),
-        ],
-      ),
-    );
-  }
-
-  Widget buildRoom(int roomIndex) {
-    List<String> roomImages = [
+    final roomImages = [
       'assets/joy_room.jpg',
       'assets/sorrow_room.jpg',
       'assets/anger_room.jpg',
@@ -47,81 +116,286 @@ class _EscapeGameScreenState extends State<EscapeGameScreen> {
       'assets/special_room.jpg', // 特殊部屋の画像
     ];
 
-    // 特殊部屋にいる場合は、特別なアクションを実行
-    if (roomIndex == 4) {
-      print('特殊部屋に入った！');
-      // 特殊なアクションを実行する処理
-      // ...
-    }
+    final room = Room(
+      imageAssetPath: roomImages[widget.currentRoomId],
+      roomId: widget.currentRoomId,
+    );
 
-    return GestureDetector(
-      onTap: () {
-        // 特殊部屋にいる場合は、何も処理しない
-      },
-      child: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(roomImages[roomIndex]),
-            fit: BoxFit.contain,
+    final roomDoor = RoomDoor(prevRoomId: widget.currentRoomId - 1);
+    final navigationButtons = NavigationButtons(currentRoomId: widget.currentRoomId);
+    final clickableArea = ClickableArea(currentRoomId: widget.currentRoomId);
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          room,
+          if (widget.currentRoomId > 0) roomDoor, // 部屋が0以外の時にのみ表示
+          navigationButtons,
+          clickableArea, // joy_roomでのみ表示
+          if (widget.currentRoomId == 4) SpecialRoom(), // 特殊部屋用のウィジェット
+        ],
+      ),
+    );
+  }
+}
+
+class SpecialRoom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        // 窓
+        Positioned(
+          top: 50,
+          left: 50,
+          child: ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    content: Image.asset('assets/window.jpg'),
+                  );
+                },
+              );
+            },
+            child: Text('窓'),
           ),
         ),
-      ),
-    );
-  }
-
-  Widget buildRoomDoor() {
-    return Positioned(
-      top: MediaQuery.of(context).size.height / 2 - 20,
-      left: 10,
-      child: GestureDetector(
-        onTap: () {
-          int prevRoom = currentRoom - 1;
-          if (prevRoom < 0) {
-            prevRoom = 3; // 部屋の数 - 1
-          }
-          setState(() {
-            currentRoom = prevRoom;
-          });
-        },
-        child: Icon(Icons.arrow_back, color: Colors.red),
-      ),
-    );
-  }
-
-  Widget buildNavigationButtons() {
-    return Positioned(
-      top: MediaQuery.of(context).size.height / 2 - 20,
-      right: 10,
-      child: Visibility(
-        visible: currentRoom != 4, // 特殊部屋ではない場合は表示
-        child: GestureDetector(
-          onTap: () {
-            int nextRoom = (currentRoom + 1) % 4; // 部屋の数
-            setState(() {
-              currentRoom = nextRoom;
-            });
-          },
-          child: Icon(Icons.arrow_forward, color: Colors.red),
+        // ポスター
+        Positioned(
+          top: 50,
+          right: 50,
+          child: ElevatedButton(
+            onPressed: () {
+              showPosterDialog(context);
+            },
+            child: Text('ポスター'),
+          ),
         ),
-      ),
+        // ゴミ箱
+        Positioned(
+          bottom: 50,
+          left: 50,
+          child: ElevatedButton(
+            onPressed: () {
+              showTrashDialog(context);
+            },
+            child: Text('ゴミ箱'),
+          ),
+        ),
+        // ぬいぐるみ
+        Positioned(
+          bottom: 50,
+          right: 50,
+          child: ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    content: Image.asset('assets/teddy_bear.jpg'),
+                  );
+                },
+              );
+            },
+            child: Text('ぬいぐるみ'),
+          ),
+        ),
+        // 机
+        Center(
+          child: ElevatedButton(
+            onPressed: () {
+              showDeskDialog(context);
+            },
+            child: Text('机'),
+          ),
+        ),
+      ],
     );
   }
 
-  Widget buildClickableArea() {
-    return GestureDetector(
-      onTap: () {
-        // 画像内の特定の部分をクリックしたときの処理
-        // 特殊部屋へ移動
-        setState(() {
-          currentRoom = 4; // 特殊部屋の番号
-        });
-      },
-      child: Container(
-        width: 100,
-        height: 100,
-        color: Colors.red.withOpacity(0.5),
-        margin: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width / 4, 50, 0, 0), // 画像内のクリック領域
+  void showPosterDialog(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => PosterScreen(),
+    ));
+  }
+
+  void showTrashDialog(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => TrashScreen(),
+    ));
+  }
+
+  void showDeskDialog(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => DeskScreen(),
+    ));
+  }
+}
+
+class PosterScreen extends StatefulWidget {
+  @override
+  _PosterScreenState createState() => _PosterScreenState();
+}
+
+class _PosterScreenState extends State<PosterScreen> {
+  int imageIndex = 0;
+  final List<String> posterImages = [
+    'assets/poster1.jpg',
+    'assets/poster2.jpg',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(child: Image.asset(posterImages[imageIndex])),
+          Positioned(
+            bottom: 20,
+            left: 20,
+            child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  imageIndex = (imageIndex + 1) % posterImages.length;
+                });
+              },
+              child: Text('次へ'),
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class TrashScreen extends StatefulWidget {
+  @override
+  _TrashScreenState createState() => _TrashScreenState();
+}
+
+class _TrashScreenState extends State<TrashScreen> {
+  int imageIndex = 0;
+  final List<String> trashImages = [
+    'assets/trash1.jpg',
+    'assets/trash2.jpg',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(child: Image.asset(trashImages[imageIndex])),
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  imageIndex = (imageIndex + 1) % trashImages.length;
+                });
+              },
+              child: Text('次へ'),
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            left: 20,
+            child: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DeskScreen extends StatefulWidget {
+  @override
+  _DeskScreenState createState() => _DeskScreenState();
+}
+
+class _DeskScreenState extends State<DeskScreen> {
+  int imageIndex = 0;
+  final List<String> deskImages = [
+    'assets/desk1.jpg',
+    'assets/desk2.jpg',
+    'assets/desk3.jpg',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(child: Image.asset(deskImages[imageIndex])),
+          Positioned(
+            bottom: 20,
+            left: 20,
+            child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  imageIndex = (imageIndex == 0) ? 1 : 0;
+                });
+              },
+              child: Text('左へ'),
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  imageIndex = (imageIndex == 0) ? 2 : 0;
+                });
+              },
+              child: Text('右へ'),
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            child: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,  // アプリケーション全体のテーマを設定
+      ),
+      home: EscapeGameScreen(currentRoomId: 0), // 最初の画面としてEscapeGameScreenを指定
     );
   }
 }
